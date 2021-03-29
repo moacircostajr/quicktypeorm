47119c7122950d5f864861121866316ddf003d3c
Moacir Costa <moacircostajr@gmail.com>
2021-03-28 22:31:58 -0300

    muda o metodo de execução dos testes

--- src/factory.spec.ts
+++ src/factory.spec.ts
@@ -5,19 +5,19 @@ import CoreStudent from './core/core_student'
 import Factory from './factory'
 
 describe('Factory', () => {
-  test('if FACTORY returns instance of CoreClass', () => {
+  it('should return instance of CoreClass', () => {
     const coreClass: CoreClass = Factory.getNewCoreClass()
     expect(coreClass).toEqual(new CoreClass())
   })
-  test('if FACTORY returns instance of CoreContent', () => {
+  it('should return instance of CoreContent', () => {
     const coreContent: CoreContent = Factory.getNewCoreContent()
     expect(coreContent).toEqual(new CoreContent())
   })
-  test('if FACTORY returns instance of CoreLesson', () => {
+  it('should return instance of CoreLesson', () => {
     const coreLesson: CoreLesson = Factory.getNewCoreLesson()
     expect(coreLesson).toEqual(new CoreLesson())
   })
-  test('if FACTORY returns instance of CoreStudent', () => {
+  it('should return instance of CoreStudent', () => {
     const coreStudent: CoreStudent = Factory.getNewCoreStudent()
     expect(coreStudent).toEqual(new CoreStudent())
   })
